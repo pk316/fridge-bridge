3f544847af5481719632ccb2a00f124121f4073d
kryseno <kryseno@gmail.com>
2017-10-11 12:36:33 -0700

    added food youtube vids into add_vids_to_carousel(), also added a food carousel in html. renamed each carousel id to refer to either food or drink. working on condensing code

--- youtube.js
+++ youtube.js
@@ -6,7 +6,7 @@ $(document).ready(initializeApp);
  * initializes the application, adds click handlers to submit button
  */
 function initializeApp() {
-    $('#drinksButton').click(add_vids_to_carousel);
+    $('#drinksButton, #foodButton').click(add_vids_to_carousel);
     // $('.html5-video-container').click(stopScroll);
 }
 /***************************************************************************************************
@@ -18,33 +18,62 @@ function initializeApp() {
  */
 function add_vids_to_carousel() {
     console.log('click initiated');
-    $(".item").empty();
-    var searchTerm = $('#drinkInput').val() + '';
-    console.log(searchTerm);
-    var data_object = {
-        q: searchTerm + ' drink recipe tutorial',
-        maxResults: 5
-    };
-    $.ajax({
-        dataType: 'json',
-        method: 'post',
-        url: 'http://s-apis.learningfuze.com/hackathon/youtube/search.php',
-        data: data_object,
-        success: function(result) {
-            console.log('ajax call success');
-            for (var i=0; i<result.video.length; i++) {
-                console.log(result.video[i].id);
-                $(".carousel_container").removeClass('hidden');
-                var videosList = $("<iframe>",{
-                    width: '85%',
-                    height: 315,
-                    // enablejsapi: true,
-                    src: 'https://www.youtube.com/embed/' + result.video[i].id
-                });
-                $('#video'+i).append(videosList);
+    console.log(this);
+    if ($(this).attr('id') === 'drinksButton') {
+        $(".drinksItem").empty();
+        var drinkSearchTerm = $('#drinkInput').val() + '';
+        console.log(drinkSearchTerm);
+        var drinkDataObject = {
+            q: drinkSearchTerm + ' drink recipe tutorial',
+            maxResults: 5
+        };
+        $.ajax({
+            dataType: 'json',
+            method: 'post',
+            url: 'http://s-apis.learningfuze.com/hackathon/youtube/search.php',
+            data: drinkDataObject,
+            success: function (result) {
+                console.log('ajax call success');
+                for (var i = 0; i < result.video.length; i++) {
+                    console.log(result.video[i].id);
+                    $("#drinks").removeClass('hidden');
+                    var videosList = $("<iframe>", {
+                        width: '85%',
+                        height: 315,
+                        src: 'https://www.youtube.com/embed/' + result.video[i].id
+                    });
+                    $('#drinksVideo' + i).append(videosList);
+                }
             }
-        }
-    });
+        });
+    } else {
+        $(".foodItem").empty();
+        var foodSearchTerm = $('#foodInput').val() + '';
+        console.log(foodSearchTerm);
+        var foodDataObject = {
+            q: foodSearchTerm + ' recipe tutorial',
+            maxResults: 5
+        };
+        $.ajax({
+            dataType: 'json',
+            method: 'post',
+            url: 'http://s-apis.learningfuze.com/hackathon/youtube/search.php',
+            data: foodDataObject,
+            success: function (result) {
+                console.log('ajax call success');
+                for (var i = 0; i < result.video.length; i++) {
+                    console.log(result.video[i].id);
+                    $("#food").removeClass('hidden');
+                    var videosList = $("<iframe>", {
+                        width: '85%',
+                        height: 315,
+                        src: 'https://www.youtube.com/embed/' + result.video[i].id
+                    });
+                    $('#foodVideo' + i).append(videosList);
+                }
+            }
+        });
+    }
 }
 
 // function stopScroll() {
@@ -54,28 +83,3 @@ function add_vids_to_carousel() {
 
 // on click, data-interval false
 // on left & right, data-interval true
-
-// $('iframe').on('play', function () {
-//     $("#youtube_carousel").carousel('pause');
-// });
-// $('iframe').on('stop pause ended', function () {
-//     $("#youtube_carousel").carousel();
-// });
-
-// $('.owl-carousel').owlCarousel({
-//     items:1,
-//     merge:true,
-//     loop:true,
-//     margin:10,
-//     video:true,
-//     lazyLoad:false,
-//     center:true,
-//     responsive:{
-//         480:{
-//             items:2
-//         },
-//         600:{
-//             items:4
-//         }
-//     }
-// });
