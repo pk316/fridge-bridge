1d630849149259cacc6ed80af803f9b0db53ecef
pk316 <pr.khiev@gmail.com>
2017-10-11 17:42:24 -0700

    replaced arrays with object

--- food.js
+++ food.js
@@ -41,23 +41,13 @@ function getRecipe() {
             'q': ingredient.recipe,
         },
         success: function (result) {
-            var resultObj ={};
-            var imageArray = [];
+            var recipeObj ={};
             var recipeUrlArray = [];
-            var displayNameArray = [];
-            var ingredientsArray = [];
             var counter = result.matches.length
             console.log(result.matches);
             for (var i = 0; i < result.matches.length; i++) {
-                var recipeObj = result.matches;
-                var recipeImage = recipeObj[i].imageUrlsBySize['90'];
-                var displayName = recipeObj[i].sourceDisplayName;
+                recipeObj = result.matches;
                 var recipeId = recipeObj[i].id;
-                var ingredients = recipeObj[i].ingredients;
-                imageArray.push(recipeImage);
-                ingredientsArray.push(ingredients);
-                displayNameArray.push(displayName);
-                console.log(ingredients);
 
                 $.ajax({
                     url: 'http://api.yummly.com/v1/api/recipe/' + recipeId + '?_app_id=fffebcbc&_app_key=34aa6c71c566decd872142c93f381916',
@@ -69,7 +59,7 @@ function getRecipe() {
                         recipeUrlArray.push(recipeUrl);
                         counter--;
                         if (counter === 0 ){
-                            allCallsDone(resultObj, imageArray, recipeUrl, displayNameArray, ingredientsArray)
+                            allCallsDone(recipeObj)
                         }
                     }
                 })
@@ -82,34 +72,29 @@ function getRecipe() {
 }
 /***************************************************************************************************
  * allCallsDone - call Ajax calls are complete
- * @params {imageArray, recipeUrlArray, displayName}
+ * @params {resultObj}
  * @returns  {undefined}
  *
  */
-function allCallsDone(resultObj, imageArray, recipeUrlArray, displayNameArray,ingredientsArray){
-    resultObj.images = imageArray;
-    resultObj.links = recipeUrlArray;
-    resultObj.names = displayNameArray;
-    resultObj.ingredients= ingredientsArray;
+function allCallsDone(recipeObj){
+
     console.log('all calls have completed');
-    renderImage(resultObj);
+    renderImage(recipeObj);
 }
 /***************************************************************************************************
  * renderImage
- * @params {imageArray, recipeUrlArray, displayName}
+ * @params {resultObj}
  * @returns  {undefined}
  *
  */
-function renderImage(resultObj) {
+function renderImage(recipeObj){
     console.log('render');
-    for (var i = 0; i < resultObj.images.length; i++) {
-        var imageLink = $('<a>').attr('href', resultObj.links[i]);
-        var imageOfDish = $('<img>').attr('src', resultObj.images[i]);
-        // var ingredients = $('<p>').text(resultObj.ingredients[i]);
-        var imageDiv = $('<div>').append($(imageLink).prepend(imageOfDish)).append($('<p>').text(resultObj.names[i] + ' - ' + (resultObj.ingredients[i])));
-        $('.recipeArea').append(imageDiv);
-
+    for ( i in recipeObj){
+            var recipeArrayItem = recipeObj[i].id.split('-');
+            var recipeArray = recipeArrayItem.splice(0,recipeArrayItem.length-1);
+            var recipeId = recipeArray.join(' ')
+            var imageOfDish = $('<img>').attr('src',recipeObj[i].imageUrlsBySize["90"]);
+            var imageDiv = $('<div>').append($(imageOfDish)).append($('<p>').text(recipeId + ' by ' + recipeObj[i].sourceDisplayName + ' - '+recipeObj[i].ingredients));
+            $('.recipeArea').append(imageDiv);
     }
-
-
 }
