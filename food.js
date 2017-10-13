64657ed2dba5e8a561502f93635b73e27e35b6af
pk316 <pr.khiev@gmail.com>
2017-10-12 23:39:01 -0700

    rewrote food.js functions

--- food.js
+++ food.js
@@ -4,12 +4,15 @@
  * @returns: {undefined} none
  * initializes the application, */
 
-$(document).ready(initializeApp)
+$(document).ready(initializeApp);
 
 function initializeApp(){
     addClickHandler();
-
 }
+/***************************************************************************************************
+ * Global Variables
+ */
+var counter = 0;
 /***************************************************************************************************
  * addClickHandlers
  * @params {undefined}
@@ -19,7 +22,7 @@ function initializeApp(){
 function addClickHandler(){
     $('#submitFood').click(function () {
         getRecipe();
-    })
+    });
     searchAgain();
     backToResult();
 }
@@ -29,25 +32,25 @@ function addClickHandler(){
  * @returns  {undefined}
  * resets search bar and clears all list values
  */
-function searchAgain() {
+function searchAgain(){
     $('.searchAgain').click(function() {
-        console.log('search again was pressed');
-        $('.recipeList').css('display', 'none');
-        $('.recipe div p').text('');
-        $('.recipe').css('display', 'none');
-        $('.inputFood').val('');
+        console.log('search again clicked');
+        $('.recipeList .recipe').css('display', 'none');
+        $('.recipe').text('');
+        $('#foodInput').val('');
     });
 }
 /***************************************************************************************************
  * backToResult
  * @params {undefined}
  * @returns  {undefined}
- * after user clicks on drink name and data comes up, clicking back to list will move the page back to drink list
+ *
  */
 function backToResult() {
-    var recipe = $('.recipe');
-    var recipeList = $('.recipeList');
     $('.backToList').click(function() {
+        console.log('back to results clicked');
+        var recipe = $('.recipe');
+        var recipeList = $('.recipeList');
         if (recipe.css('display') !== 'none' && recipeList.css('display') === 'none') {
             $('.recipe').css('display', 'none');
             $('.recipeList').show();
@@ -77,26 +80,11 @@ function getRecipe() {
         },
         success: function (result) {
             var recipeObj ={};
-            var recipeUrlArray = [];
-            var counter = result.matches.length
+
             console.log(result.matches);
             for (var i = 0; i < result.matches.length; i++) {
-                recipeObj = result.matches;
-                var recipeId = recipeObj[i].id;
-                $.ajax({
-                    url: 'http://api.yummly.com/v1/api/recipe/' + recipeId + '?_app_id=fffebcbc&_app_key=34aa6c71c566decd872142c93f381916',
-                    dataType: 'JSON',
-                    method: 'GET',
-                    success: function (data) {
-                        console.log('second ajax', data.source);
-                        var recipeUrl = data.source.sourceRecipeUrl;
-                        recipeUrlArray.push(recipeUrl);
-                        counter--;
-                        if (counter === 0 ){
-                            allCallsDone(recipeObj, recipeUrlArray);
-                        }
-                    }
-                })
+                recipeObj = result.matches[i];
+                renderRecipe(recipeObj);
             }
         },
         error: function (err) {
@@ -105,68 +93,82 @@ function getRecipe() {
     })
 }
 /***************************************************************************************************
- * allCallsDone - all Ajax calls are complete, call next function
- * @params {recipeObj, recipeUrlArray}
- * @returns  {undefined}
- *
- */
-function allCallsDone(recipeObj, recipeUrlArray){
-    console.log('all calls are complete');
-    renderRecipe(recipeObj, recipeUrlArray);
-}
-/***************************************************************************************************
  * renderRecipe
- * @params {recipeObj, recipeUrlArray}
+ * @params {recipeObj}
  * @returns  {undefined}
  *
  */
-function renderRecipe(recipeObj, recipeUrlArray){
-   var ingredientObj = {};
+function renderRecipe(recipeObj){
     $('.recipeList').show();
     console.log('render');
-    for ( i in recipeObj){
-            var recipeArrayItem = recipeObj[i].id.split('-');
-            var recipeArray = recipeArrayItem.splice(0,recipeArrayItem.length-1);
-            var recipeId = recipeArray.join(' ');
-            var ingredients = recipeObj[i].ingredients;
-            ingredientObj[i] = ingredients;
-                if ( i < 5){
-                    var imageOfDish = $('<img>').attr('src',recipeObj[i].imageUrlsBySize["90"]).click(function(){
-                        renderIngredients(recipeObj, ingredientObj, recipeUrlArray);
-                    });
-                    var imageDiv = $('<div>').append($(imageOfDish)).append($('<p>').text(recipeId + ' by ' + recipeObj[i].sourceDisplayName));
-                    $('.rL1').append(imageDiv);
-                } else{
-                    var imageOfDish = $('<img>').attr('src',recipeObj[i].imageUrlsBySize["90"]).click(function(){
-                        renderIngredients(recipeObj, ingredientObj, recipeUrlArray);
-                    });
-                    var imageDiv = $('<div>').append($(imageOfDish)).append($('<p>').text(recipeId + ' by ' + recipeObj[i].sourceDisplayName));
-                    $('.rL2').append(imageDiv);
-                }
-    }
-
-}
-/***************************************************************************************************
- * renderIngredients
- * @params {recipeObj, ingredientObj, recipeUrlArray}
- * @returns  {undefined}
- *
- */
-function renderIngredients( recipeObj, ingredientObj, recipeUrlArray) {
-    $('.recipe').show();
-    $('.recipeList').hide();
-
-    var recipeArrayItem = recipeObj[i].id.split('-');
+    var recipeArrayItem = recipeObj.id.split('-');
     var recipeArray = recipeArrayItem.splice(0,recipeArrayItem.length-1);
     var recipeId = recipeArray.join(' ');
 
-        var title = $('<p>').text(recipeId+ ' by ' + recipeObj[i].sourceDisplayName);
-        $('.recipe h2').append(title);
-        var imageOfDish = $('<img>').attr('src',recipeObj[i].imageUrlsBySize["90"]);
-        var imageDiv = $('<div>').append($(imageOfDish));
-        $('.recipePhoto').append(imageDiv);
-        var instructions = $('<a>').attr('href',recipeUrlArray[i]).text(recipeUrlArray[i]);
-        $('.recipe .instructions p').append(instructions);
-        var ingredientList = $('<p>').text(ingredientObj[i]);
-        $('.recipe .foodIngredients').append(ingredientList);
+    if ( counter  < 5){
+        var imageOfDish = $('<img>').attr('src',recipeObj.imageUrlsBySize["90"]).click(function(){
+            getInstructionUrl(recipeObj);
+        });
+        var imageDiv = $('<div>').append($(imageOfDish)).append($('<p>').text(recipeId + ' by ' + recipeObj.sourceDisplayName));
+        $('.rL1').append(imageDiv);
+    } else{
+        var imageOfDish = $('<img>').attr('src',recipeObj.imageUrlsBySize["90"]).click(function(){
+            getInstructionUrl(recipeObj);
+        });
+        var imageDiv = $('<div>').append($(imageOfDish)).append($('<p>').text(recipeId + ' by ' + recipeObj.sourceDisplayName));
+        $('.rL2').append(imageDiv);
+    }
+    counter++;
+}
+/***************************************************************************************************
+ * ajax call to get recipe instructions using recipe id from getRecipe ajax call
+ * @params {recipeObj}
+ * @returns  {undefined}
+ *
+ */
+function getInstructionUrl(recipeObj){
+    $.ajax({
+        url: 'http://api.yummly.com/v1/api/recipe/' + recipeObj.id+ '?_app_id=fffebcbc&_app_key=34aa6c71c566decd872142c93f381916',
+        dataType: 'JSON',
+        method: 'GET',
+        success: function (data) {
+            console.log('second ajax', data.source);
+            var recipeUrl = data.source.sourceRecipeUrl;
+            renderIngredients(recipeObj, recipeUrl);
+        }
+    })
+}
+/***************************************************************************************************
+ * renderIngredients
+ * @params {recipeObj, recipeUrl}
+ * @returns  {undefined}
+ *
+ */
+function renderIngredients( recipeObj, recipeUrl) {
+    $('.recipe').show();
+    $('.recipeList').css('display','none');
+    var recipeArrayItem = recipeObj.id.split('-');
+    var recipeArray = recipeArrayItem.splice(0,recipeArrayItem.length-1);
+    var recipeId = recipeArray.join(' ');
+
+    var title = $('<p>').text(recipeId+ ' by ' + recipeObj.sourceDisplayName);
+    $('.recipe h2').append(title);
+
+    var imageOfDish = $('<img>').attr('src',recipeObj.imageUrlsBySize["90"]);
+    var imageDiv = $('<div>').append($(imageOfDish));
+    $('.recipePhoto').append(imageDiv);
+
+    var instructions = $('<a>').attr('href',recipeUrl).text(recipeUrl);
+    $('.recipe .instructions p').append(instructions);
+
+    if (typeof(recipeObj) === 'object') {
+        var ingredients = recipeObj.ingredients;
+        for (var j = 0; j < ingredients.length; j++) {
+            var ingredientList = $('<li>', {
+                text: ingredients[j]
+            });
+            var listOfIngredients = $('.foodIngredients > ul').append(ingredientList);
+        }
+    }
+    $('.recipe .foodIngredients').append(listOfIngredients);
 }
